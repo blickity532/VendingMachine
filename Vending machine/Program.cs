63f4157d290580f8719ea9ffe6c62545f59aa380
blake532 <blickity@gmail.com>
2025-02-03 23:58:58 -0800

    Initial commit: Add basic vending machine functionality

---
+++ Vending machine/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowMenu();
+            int choice = GetUserChoice();
+            VendItem(choice);
+        }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine("Welcome to the Vending Machine!");
+            Console.WriteLine("Please select an item:");
+            Console.WriteLine("1. Soda");
+            Console.WriteLine("2. Candy");
+        }
+
+        static int GetUserChoice()
+        {
+            Console.Write("Enter the number of your choice: ");
+            string? input = Console.ReadLine();
+            int choice;
+            while (!int.TryParse(input, out choice) || choice < 1 || choice > 2)
+            {
+                Console.WriteLine("Invalid choice. Please enter 1 or 2.");
+                input = Console.ReadLine();
+            }
+            return choice;
+        }
+
+        static void VendItem(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Vending Soda...");
+                    break;
+                case 2:
+                    Console.WriteLine("Vending Candy...");
+                    break;
+            }
+        }
+    }
+}
